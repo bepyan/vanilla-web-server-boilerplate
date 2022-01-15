7fff6a739900042540ba72c4b6e0d2bd938daa05
bepyan <bepyan@naver.com>
2022-01-16 01:19:35 +0900

    feat. prod webpack  성능 최적화

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,6 +1,6 @@
 import "../styles/index.scss";
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
   console.log("[ 개발모드로 실행중 ]");
 }
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
@@ -6,6 +8,15 @@ const HTML_TEMPLATE = "public/production.html";
 
 module.exports = merge(common, {
   mode: "production",
+  // devtool: "source-map", // 배포 디버깅용
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "홈",
